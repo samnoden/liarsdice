b25ba6427684512240ed9ae8f5cba6f35e4f636f
Sam Noden <nodensam@gmail.com>
2023-06-22 17:55:10 -0400

    added in new file in server folder for refactoring functions

---
+++ server/functions.js
@@ -0,0 +1,4 @@
+// this folder will hold all the functions
+// required by the server file, index.js within this folder
+// every function will include a strict description
+// of what it is returning and its purpose
